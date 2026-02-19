f3847b561aba2c8a21bfa736c2fc00e20569ce8a
richardmcmanusjr <richardmcmanusjr@gmail.com>
2026-02-19 11:55:26 -0800

    Updated Timer

--- plato.js
+++ plato.js
@@ -302,9 +302,8 @@ function getNextScheduledRun() {
     
     // 0 = Sunday, 1 = Monday, 2 = Tuesday, 3 = Wednesday, 4 = Thursday, 5 = Friday, 6 = Saturday
     const scheduledDays = {
-        2: { hour: 6, minute: 30 },   // Tuesday
-        3: { hour: 17, minute: 0 },   // Wednesday (5:00 PM)
-        4: { hour: 6, minute: 30 }    // Thursday
+        0: { hour: 9, minute: 00 },    // Sunday (6:30 AM)
+        3: { hour: 17, minute: 30 },   // Wednesday (5:30 PM)
     };
 
     // Check the next 8 days for a scheduled run
